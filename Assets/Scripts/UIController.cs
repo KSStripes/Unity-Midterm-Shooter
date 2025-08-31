d76e8dbb9c74fd5ddc0af83f69444b69753a23f9
KSStripes <kristin.schumann@gmail.com>
2025-08-31 16:59:32 +0200

    Lesson 7 end - added TMP, final UI for weapon switching and healthbar and blood damage screen

--- Assets/Scripts/UIController.cs
+++ Assets/Scripts/UIController.cs
@@ -7,25 +7,28 @@ using UnityEngine.UI;
 public class UIController : MonoBehaviour
 {
     private HealthSystem playerHealth;
-    //private ShootAbility shootAbility;
+    private ShootAbility shootAbility;
     [SerializeField] private Slider healthBar;
 
     [SerializeField] private TextMeshProUGUI healthPercentText;
-    //[SerializeField] private GameObject bulletEnabled;
-    //[SerializeField] private GameObject rocketEnabled;
+    [SerializeField] private GameObject bulletEnabled;
+    [SerializeField] private GameObject rocketEnabled;
 
     private void Start()
     {
         playerHealth = PlayerInput.Instance.GetComponent<HealthSystem>();
-        //shootAbility = PlayerInput.Instance.GetComponent<ShootAbility>();
+        shootAbility = PlayerInput.Instance.GetComponent<ShootAbility>();
 
-        //shootAbility.OnChangeStrategy += SelectShootingStrategy;
+        shootAbility.OnChangeStrategy += SelectShootingStrategy;
 
         playerHealth.OnHealthChanged += UpdateHealthSlider;
         playerHealth.OnDeath += DisplayDeathScreen;
 
         healthBar.maxValue = playerHealth.GetMaxHealth();
         healthBar.value = playerHealth.GetCurrentHealth();
+        healthPercentText.text = "100%";
+        bulletEnabled.SetActive(false); // not grey out bullet
+        rocketEnabled.SetActive(true); // grey out rocket at start
     }
 
     private void DisplayDeathScreen()
@@ -33,11 +36,13 @@ public class UIController : MonoBehaviour
         Debug.Log("Player has Died");
     }
 
+    // Vignette screen based on damage taken
     private void ChangeDamageEffect()
     {
         Volume volume = FindFirstObjectByType<Volume>();
 
-        if(volume.profile.TryGet(out Vignette vignette))
+        // link health with vignette
+        if (volume.profile.TryGet(out Vignette vignette))
         {
             float normalized = healthBar.value / healthBar.maxValue; // returns value between 0 and 1
             float inverted = 1f - normalized;
@@ -48,21 +53,21 @@ public class UIController : MonoBehaviour
     private void UpdateHealthSlider(float value)
     {
         healthBar.value = value;
-        //healthPercentText.text = (int)(healthBar.value / healthBar.maxValue * 100) + "%";
-        ChangeDamageEffect();
+        healthPercentText.text = (int)(healthBar.value / healthBar.maxValue * 100) + "%";
+        ChangeDamageEffect(); // spawn blood effect
     }
 
-    // private void SelectShootingStrategy(int index)
-    // {
-    //     if (index == 0)
-    //     {
-    //         bulletEnabled.SetActive(false);
-    //         rocketEnabled.SetActive(true);
-    //     }
-    //     else if (index == 1)
-    //     {
-    //         bulletEnabled.SetActive(true);
-    //         rocketEnabled.SetActive(false);
-    //     }
-    // }
+    private void SelectShootingStrategy(int index)
+    {
+        if (index == 0)
+        {
+            bulletEnabled.SetActive(false);
+            rocketEnabled.SetActive(true);
+        }
+        else if (index == 1)
+        {
+            bulletEnabled.SetActive(true);
+            rocketEnabled.SetActive(false);
+        }
+    }
 }
